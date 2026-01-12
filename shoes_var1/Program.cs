c70371ab5bf67d63ea7f70bd5de85d040554cbec
Игорь Шапкин <shapkin.igorek@gmail.com>
2026-01-12 10:22:55 +0300

    Добавьте файлы проекта.

---
+++ shoes_var1/Program.cs
@@ -0,0 +1,20 @@
+﻿using shoes_var1.AppForms;
+using System;
+using System.Windows.Forms;
+
+namespace shoes_var1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+        }
+    }
+}
